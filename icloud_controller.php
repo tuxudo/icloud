eaced2bfeb4db32c458fcb410429611b82b8cddf
tuxudo <tuxudo@users.noreply.github.com>
2023-03-29 13:36:01 -0400

    Update icloud_controller.php

--- icloud_controller.php
+++ icloud_controller.php
@@ -533,6 +533,9 @@ class Icloud_controller extends Module_controller
     **/
     public function get_tab_data($serial_number = '')
     {
+        // Remove non-serial number characters
+        $serial_number = preg_replace("/[^A-Za-z0-9_\-]]/", '', $serial_number);
+
         $obj = new View();
 
         if (! $this->authorized()) {
